b396dceffffae18494b8accd48d9ba319e032fe9
Maison <maisonflynn@mail.com>
2024-10-05 12:11:10 -0400

    Esskeetit!

---
+++ Bender.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+	console.log("Esskeetit!");
+});
+
+client.login(process.env.TOKEN);
