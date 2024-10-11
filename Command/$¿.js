c616e8dff5d3fcef242f7a50a0ff4d544f374dbf
Maison <maisonflynn@mail.com>
2024-10-11 15:58:24 -0400

    (づ •. •)?

--- Command/$¿.js
+++ Command/$¿.js
@@ -1,14 +1,19 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = async function Help(message) {
-    const CMD = 
-      '```\n' +
-      '$?\n' +
-      '$!\n' +
-      '$#\n' +
-      '$$\n' +
-      '$-\n' +
-      '$+\n' +
-      '$<10-1000/10>\n' +
-      '```';
-    
-    await message.reply(CMD);
-};  
+    const msg = new EmbedBuilder()
+    .setTitle('CMD')
+        .setDescription(
+            '`$?`\n' +
+            '`$!`\n' +
+            '`$#`\n' +
+            '`$$`\n' +
+            '`$-`\n' +
+            '`$+`\n' +
+            '`$<10-1000/10>`'
+        )
+        .setColor('#2B2D31')
+        .setFooter({ text: 'https://bicyclecards.com/how-to-play/blackjack' });
+        
+    await message.reply({ embeds: [msg] });
+};
