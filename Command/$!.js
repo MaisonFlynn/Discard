801a681acd0c2bd14f12c382843da5071f869e97
Maison Flynn <maisonflynn@mail.com>
2025-01-11 17:50:59 -0500

    Swiffer Wet Jet

--- Command/$!.js
+++ Command/$!.js
@@ -29,7 +29,7 @@ module.exports = async function Claim(I, P) {
 
     const msg = new EmbedBuilder()
         .setColor('#2B2D31')
-        .setDescription(`${dong}\n${shmoney}`);
+        .setDescription(`${title}${dong}\n${shmoney}`);
 
     const btn = Btn1(P.Msg);
 
